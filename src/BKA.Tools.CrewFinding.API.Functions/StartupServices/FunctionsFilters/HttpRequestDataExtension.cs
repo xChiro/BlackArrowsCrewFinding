f593e83b0818b9b448117e57828b608ebcd81e5c
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-04-29 16:54:53 -0600

    Update return type in HttpRequestDataExtension
    The return type of the method GetHttpRequestData has been updated to nullable, providing more flexibility during data retrieval. Also, implicit typing is now used for initializing variables, making the code cleaner and easier to maintain.

--- src/BKA.Tools.CrewFinding.API.Functions/StartupServices/FunctionsFilters/HttpRequestDataExtension.cs
+++ src/BKA.Tools.CrewFinding.API.Functions/StartupServices/FunctionsFilters/HttpRequestDataExtension.cs
@@ -2,17 +2,19 @@ namespace BKA.Tools.CrewFinding.API.Functions.StartupServices.FunctionsFilters;
 
 public static class HttpRequestDataExtension
 {
-    public static HttpRequestData GetHttpRequestData(this FunctionContext functionContext)
+    public static HttpRequestData? GetHttpRequestData(this FunctionContext functionContext)
     {
         try
         {
-            KeyValuePair<Type, object> keyValuePair =
-                functionContext.Features.SingleOrDefault(f => f.Key.Name == "IFunctionBindingsFeature");
-            object functionBindingsFeature = keyValuePair.Value;
-            Type type = functionBindingsFeature.GetType();
+            var keyValuePair = functionContext.Features.SingleOrDefault(f => f.Key.Name == "IFunctionBindingsFeature");
+
+            var functionBindingsFeature = keyValuePair.Value;
+            var type = functionBindingsFeature.GetType();
+
             var inputData =
                 type.GetProperties().Single(p => p.Name == "InputData").GetValue(functionBindingsFeature) as
                     IReadOnlyDictionary<string, object>;
+            
             return inputData?.Values.SingleOrDefault(o => o is HttpRequestData) as HttpRequestData;
         }
         catch
