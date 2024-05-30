7e67509f98d0777fe735bfebf8155c40285ab626
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-05-29 18:53:05 -0600

    Refactor classes 'KeySecretProviderBuilder' and 'KeySecretProvider'
    The 'KeySecretProviderBuilder' and 'KeySecretProvider' classes have been modified to take parameters in their constructor directly, thus eliminating the need for private readonly fields. This leads to a more concise and cleaner code, by reducing the number of lines and improving readability.

--- src/BKA.Tools.CrewFinding.KeyVault/KeySecretProvider.cs
+++ src/BKA.Tools.CrewFinding.KeyVault/KeySecretProvider.cs
@@ -2,17 +2,10 @@ using Azure.Security.KeyVault.Secrets;
 
 namespace BKA.Tools.CrewFinding.KeyVault;
 
-public class KeySecretProvider : IKeySecretProvider
+public class KeySecretProvider(SecretClient secretClient) : IKeySecretProvider
 {
-    private readonly SecretClient _secretClient;
-
-    public KeySecretProvider(SecretClient secretClient)
-    {
-        _secretClient = secretClient;
-    }
-    
     public string GetSecret(string secretName)
     {
-        return _secretClient.GetSecret(secretName).Value.Value;
+        return secretClient.GetSecret(secretName).Value.Value;
     }
 }
--- src/BKA.Tools.CrewFinding.KeyVault/KeySecretProviderBuilder.cs
+++ src/BKA.Tools.CrewFinding.KeyVault/KeySecretProviderBuilder.cs
@@ -4,20 +4,13 @@ using Azure.Security.KeyVault.Secrets;
 
 namespace BKA.Tools.CrewFinding.KeyVault;
 
-public class KeySecretProviderBuilder
+public class KeySecretProviderBuilder(string endPoint)
 {
-    private readonly string _endPoint;
-
-    public KeySecretProviderBuilder(string endPoint)
-    {
-        _endPoint = endPoint;
-    }
-    
     public KeySecretProvider Build()
     {
         var secretClientOptions = new SecretClientOptions();
         secretClientOptions.AddPolicy(new KeyVaultProxyPolicy(), HttpPipelinePosition.PerCall);
-        var secretClient = new SecretClient(new Uri(_endPoint),
+        var secretClient = new SecretClient(new Uri(endPoint),
             new DefaultAzureCredential(),
             secretClientOptions);
 
