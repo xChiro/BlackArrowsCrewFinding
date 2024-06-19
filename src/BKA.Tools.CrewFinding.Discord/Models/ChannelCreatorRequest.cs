fe41ce51d1a518b5c9b6bd59d8d56a471f1855f0
Jorge "Chiro" Nuñez Delgado <jorge.del1991@yahoo.com.mx>
2024-06-18 19:42:15 -0600

    Reformat 'ParentId' property annotation in ChannelCreatorRequest
    The code reformats how the 'ParentId' property in the ChannelCreatorRequest model is neatly annotated on a separate line. This change improves code readability and assists in maintaining code style consistency.

--- src/BKA.Tools.CrewFinding.Discord/Models/ChannelCreatorRequest.cs
+++ src/BKA.Tools.CrewFinding.Discord/Models/ChannelCreatorRequest.cs
@@ -4,7 +4,8 @@ namespace BKA.Tools.CrewFinding.Discord.Models;
 
 public record ChannelCreatorRequest(string Name, ChannelTypes Type, string ParentId)
 {
-    [JsonPropertyName("parent_id")] public string ParentId { get; init; } = ParentId;
+    [JsonPropertyName("parent_id")] 
+    public string ParentId { get; init; } = ParentId;
 
     public ChannelTypes Type { get; init; } = Type;
 
